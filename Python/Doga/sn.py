876be9af51a66783f9eb9f57c0ecd71267373fb9
papadam23 <papadam983@gmail.com>
2022-11-22 16:53:03 +0100

    sn

---
+++ Python/Doga/sn.py
@@ -0,0 +1,82 @@
+import math
+class versenyzo:
+    def __init__(self,helyezes,nev,orszag,nyeremeny) -> None:
+        self.helyezes=helyezes
+        self.nev=nev
+        self.orszag=orszag
+        self.nyeremeny=nyeremeny
+    def __str__(self) -> str:
+        return "Név:"+self.nev+ " " "fizetése"+" "+str(self.nyeremeny)
+
+versenyzok=[]
+
+def beolvas():
+    f=open("snooker.txt", encoding="UTF8")
+    f.readline()
+    for sor in f:
+        temp=sor.strip().split(';')
+        versenyzok.append(versenyzo(int(temp[0]),temp[1],temp[2],int(temp[3])))
+    
+
+
+def f3():
+    print(f"3 feladat {len(versenyzok)}")
+
+def f4():
+    sum=0
+    for item in versenyzok:
+        sum+=item.nyeremeny
+    print(round(sum/len(versenyzok)),2)
+
+def f5():
+    maxsorszam=-1
+    max=0
+    for i in range(len(versenyzok)):
+        if versenyzok[i].orszag=="Kína"and versenyzok[i].nyeremeny>max:
+            max=versenyzok[i].nyeremeny
+            maxsorszam=i
+    print(versenyzok[maxsorszam])
+
+def f6(nemzetiseg):
+    i=0
+    while i<len(versenyzok) and versenyzok[i].orszag !=nemzetiseg:
+        i+=1
+    if i<len(versenyzok):
+        return True
+    else:
+        return False
+
+def f62():
+    vanE=False
+    for item in versenyzok:
+        if item.orszag== "Norvégia":
+            print("van")
+            vanE=True
+            break
+    if not vanE:
+        print("Nincs")
+            
+
+def f7(): #halmaz
+    halmaz=set([])
+    for item in versenyzok:
+        halmaz.add(item.orszag)
+    for item in halmaz:
+        db=szamol(item)
+        if db>4:
+            print(f"{item}: {db}")
+def szamol(orszag):
+    db=0
+    for item in versenyzok:
+        if item.orszag== orszag:
+            db+=1
+    return db
+
+
+beolvas()
+f3()
+f4()
+f5()
+vanE=f6("Norvégia")
+print(vanE)
+f7()
